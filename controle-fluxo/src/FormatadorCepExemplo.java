ac6eed28119e10892f5fb1537b7cf48264686b79
Valdirlan <1029254@gmail.com>
2024-05-06 14:49:18 -0300

    novo commit

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,19 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cep = "23765064";
+            String cepFormatado = formatarCep(cep);
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println(e.getMessage());
+            }
+    }
+    
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
